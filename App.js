f980687867ca5a8daaf106e77d92df408307d270
Manil Malla <manil.malla@softnep.com>
2023-04-05 12:37:14 +0545

    FEAT: Update 'App.js'.

--- App.js
+++ App.js
@@ -1,111 +1,77 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
 import {
+  Image,
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Player from './src/components/Player';
+import {Images} from './src/res';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+    <SafeAreaView style={styles.root}>
+      <StatusBar barStyle={'light-content'} backgroundColor={'#1B1B1B'} />
+      <View style={styles.mainContainer}>
+        <View />
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+        <View style={styles.titleContainer}>
+          <Image
+            source={Images.logo}
+            style={styles.logo}
+            resizeMode={'contain'}
+          />
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+          <Text style={styles.title}>Transient Audio Stream</Text>
+          <Text style={styles.subtitle}>
+            An example app created to replicate a bug in
+            react-native-track-player.
+          </Text>
+          <Text style={styles.subtitle}>
+            On opening the app you should hear audio playing. Close the app
+            multiple times and you will see a message like 'Transient Audio
+            Stream keeps stopping'.
+          </Text>
         </View>
-      </ScrollView>
+
+        <Player initPlay />
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  root: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  mainContainer: {
+    flex: 1,
+    backgroundColor: '#1B1B1B',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  titleContainer: {
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    padding: 30,
   },
-  highlight: {
+  title: {
+    color: '#FFFFFF',
+    fontSize: 26,
+    lineHeight: 34,
     fontWeight: '700',
+  },
+  subtitle: {
+    color: '#F0F0F0',
+    fontSize: 14,
+    lineHeight: 22,
+    fontWeight: '500',
+    textAlign: 'center',
+    marginTop: 12,
   },
 });
 
